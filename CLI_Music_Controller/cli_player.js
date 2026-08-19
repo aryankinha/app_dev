db65a86d859f419039dd8159384f4185f5aaa53a
aryankinha <aryankinha2@gmail.com>
2026-08-19 11:39:22 +0530

    feat: implement interactive CLI song selection using @inquirer/select and add project configuration files

--- CLI_Music_Controller/cli_player.js
+++ CLI_Music_Controller/cli_player.js
@@ -1,6 +1,9 @@
+#!/Users/aryankinha/.nvm/versions/node/v20.20.2/bin/node
+
 const { spawn } = require("child_process");
 const fs = require("fs");
 const path = require("path")
+const select = require('@inquirer/select').default;
 const SONG_DIR = path.join(__dirname, "song")
 
 // function listSong(dir_path) { 
@@ -19,11 +22,11 @@ const SONG_DIR = path.join(__dirname, "song")
 
 function listSong(dir_path) {
     const arr = fs.readdirSync(dir_path).filter(file => file.endsWith('.mp3'));
-    arr.forEach((song, index) => {
-        if (song) {
-            console.log(`${index}: ${song}`);
-        }
-    });
+    // arr.forEach((song, index) => {
+    //     if (song) {
+    //         console.log(`${index}: ${song}`);
+    //     }
+    // });
     return arr;
 }
 
@@ -31,10 +34,22 @@ function playSong(song) {
     const scanner = spawn("ffplay", [song]);
 }
 
-const arr = listSong(SONG_DIR);
+async function songSelector() {
+    const arr = listSong(SONG_DIR);
+    const choice = await select({
+        message: 'Select a song',
+        choices: arr.map((song) => ({
+            name: `${song}`,
+            value: song
+        })),
+    });
 
-process.stdin.on("data", (data) => {
-    const index = Number(data.toString().trim());
+    playSong(`${SONG_DIR}/${choice}`);
+}
 
-    playSong(`${SONG_DIR}/${arr[index]}`);
-});
+songSelector();
+// process.stdin.on("data", (data) => {
+//     const index = Number(data.toString().trim());
+
+//     playSong(`${SONG_DIR}/${arr[index]}`);
+// });
